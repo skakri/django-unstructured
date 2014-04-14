5d4222efe943c6dc9326a3725705d69d5b30481d
Kristaps Karlsons <kristaps.karlsons@gmail.com>
2014-04-14 18:03:00 +0300

    Version bump.

--- wiki/__init__.py
+++ wiki/__init__.py
@@ -18,4 +18,4 @@
 # You should have received a copy of the GNU General Public License
 # along with django-wiki-base. If not, see <http://www.gnu.org/licenses/>.
 
-VERSION = "0.1.0"
+VERSION = "0.2.0"
